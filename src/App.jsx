82c8fbaeebe6c3bc3cc7832426c0d7e026ed25a1
AKSUF <abu053125@gmail.com>
2023-04-26 14:06:20 +0600

    version1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-git init
 
+import "./App.css"
+import Counter from "./components/Counter";
 function App() {
     return ( 
-<div><h1>Hello</h1></div>
+
+<div><h1>Hello</h1>
+<Counter/>
+
+</div>
 
     )
 }
